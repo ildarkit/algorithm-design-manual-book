7f45582a7bce801a0653cb343bbe9afebc7f86f6
Ildar Shamiev <shamief@yandex.ru>
2023-10-07 18:30:58 +0300

    fix: counting of equal temperatures

--- leetcode_problems/54_page/solutions/daily-temperatures/src/main.rs
+++ leetcode_problems/54_page/solutions/daily-temperatures/src/main.rs
@@ -42,24 +42,32 @@ impl Solution {
         while i < temp.len() {
             skip = false;
             j = i;
-            if temp[last_skip_pos] == temp[j] && temp[j - 1] == temp[j] {
-                while j < temp.len() && temp[j - 1] == temp[j] {
-                    skip_step += 1;
-                    last_skip_pos = j;
-                    j += 1;
-                    skip = true;
+            if j < temp.len() - 2 {
+                if last_skip_pos > 0 && temp[last_skip_pos] != temp[j] && temp[j - 1] == temp[j] {
+                    c = Self::sub_interval(&temp[(j - 1)..], &mut answer[(j - 1)..]);
+                    i += c - 1;
+                    if counter == 1 {
+                        counter += c;
+                    }
                 }
-            } else if temp[j - 1] == temp[j] {
-                c = Self::sub_interval(&temp[(j - 1)..], &mut answer[(j - 1)..]);
-                i += c - 1;
-                if counter == 1 {
-                    counter += c;
+                else if temp[last_skip_pos] == temp[j] || temp[j - 1] == temp[j] && answer[j] == 0 {
+                    let mut pos = match temp[last_skip_pos] == temp[j] {
+                        true => last_skip_pos,
+                        false => j - 1,
+                    };
+                    while j < temp.len() && temp[pos] == temp[j] {
+                        skip_step += j - pos;
+                        pos = j;
+                        j += 1;
+                        skip = true;
+                    }
+                    last_skip_pos = pos;
                 }
             }
             if skip {
-                i += skip_step;
+                i = j;
                 after_skip_pos = i;
-                counter += skip_step;
+                // counter += skip_step - c;
             }
 
             c = 0;
@@ -82,13 +90,18 @@ impl Solution {
                     if n + skip_step < larger_pos {
                         skip_step += 1;
                     }
+                    let before_skip_step = larger_pos - skip_step - 1;
+                    if last_skip_pos - skip_step >= before_skip_step {
+                        skip_step += before_skip_step;
+                    }
 
+                    let mut k = last_skip_pos;
                     while skip_step > 0 {
-                        if answer[last_skip_pos] == 0 {
-                            answer[last_skip_pos] = n as i32;
+                        if answer[k] == 0 && temp[k] == temp[last_skip_pos] {
+                            answer[k] = n as i32;
                         }
                         skip_step -= 1;
-                        last_skip_pos -= 1;
+                        k -= 1;
                         n += 1;
                     }
                 }
@@ -140,15 +153,17 @@ pub fn naive_daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
 }
 
 fn main() {
-    // let mut temperatures = vec![99; 100000];
-    // temperatures[99999] = 100;
-    let temperatures = vec![94, 94, 48, 39, 66, 71, 82, 39, 94, 44, 100];
-    println!("temperatures = {:?}", temperatures);
-    let solution = Solution::daily_temperatures(temperatures.clone());
-    println!("solution: {:?}", solution);
-    let naive_solution = naive_daily_temperatures(temperatures);
-    println!("naive solution: {:?}", naive_solution);
-    assert_eq!(solution, vec![10, 9, 2, 1, 1, 1, 2, 1, 2, 1, 0]);
+    // // let mut temperatures = vec![99; 100000];
+    // // temperatures[99999] = 100;
+    // let temperatures = vec![
+    //     100, 41, 83, 57, 57, 83, 84, 57, 63, 52, 57, 78, 91, 41, 34, 75
+    // ];
+    // println!("temperatures = {:?}", temperatures);
+    // let solution = Solution::daily_temperatures(temperatures.clone());
+    // println!("solution: {:?}", solution);
+    // let naive_solution = naive_daily_temperatures(temperatures);
+    // println!("naive solution: {:?}", naive_solution);
+    // assert_eq!(solution, vec![0, 1, 4, 2, 1, 1, 6, 1, 3, 1, 1, 1, 0, 2, 1, 0]);
 
     // let temperatures = vec![
     //     vec![77,77,77,77,77,41,77,41,41,77],
@@ -170,6 +185,12 @@ fn main() {
     //     vec![100, 73, 63, 95, 42, 42, 40, 42, 42, 31, 58],
     //     vec![95, 95, 37, 79, 79, 41, 70, 47, 62, 98, 63],
     //     vec![94, 94, 48, 39, 66, 71, 82, 39, 94, 44, 100],
+    //     vec![68, 44, 69, 88, 54, 38, 54, 48, 54, 54, 92],
+    //     vec![62, 74, 54, 88, 88, 41, 75, 99, 74, 84, 86],
+    //     vec![42, 40, 67, 76, 76, 45, 61, 30, 44, 76, 76],
+    //     vec![85, 68, 85, 60, 81, 85, 85, 41, 74, 89, 91],
+    //     vec![86, 74, 77, 54, 35, 32, 34, 77, 58, 87, 75],
+    //     vec![100, 41, 83, 57, 57, 83, 84, 57, 63, 52, 57, 78, 91, 41, 34, 75],
     // ];
     // for t in temperatures {
     //     println!("temperatures = {:?}", t);
@@ -180,18 +201,18 @@ fn main() {
     //     assert_eq!(solution, naive_solution);
     // }
 
-    // let dist = Uniform::new_inclusive(30, 100);
-    // let mut rng = rand::thread_rng();
-    // for i in 0..10000 {
-    //     if i % 10 == 0 {
-    //         println!("step = {}", i);
-    //     }
-    //     let temperatures: Vec<_> = (1..=11).map(|_| dist.sample(&mut rng)).collect();
-    //     println!("temperatures = {:?}", temperatures);
-    //     let solution = Solution::daily_temperatures(temperatures.clone());
-    //     // println!("solution: {:?}", solution);
-    //     let naive_solution = naive_daily_temperatures(temperatures);
-    //     // println!("naive solution: {:?}", naive_solution);
-    //     assert_eq!(solution, naive_solution);
-    // }
+    let dist = Uniform::new_inclusive(30, 100);
+    let mut rng = rand::thread_rng();
+    for i in 0..100 {
+        if i % 10 == 0 {
+            println!("step = {}", i);
+        }
+        let temperatures: Vec<_> = (1..=10000).map(|_| dist.sample(&mut rng)).collect();
+        // println!("temperatures = {:?}", temperatures);
+        let solution = Solution::daily_temperatures(temperatures.clone());
+        // println!("solution: {:?}", solution);
+        let naive_solution = naive_daily_temperatures(temperatures);
+        // println!("naive solution: {:?}", naive_solution);
+        assert_eq!(solution, naive_solution);
+    }
 }
