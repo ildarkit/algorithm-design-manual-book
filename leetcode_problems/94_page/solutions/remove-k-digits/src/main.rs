ccd41b5ce1963084fa6bf6700f97b57e68a1e068
Ildar Shamiev <shamief@yandex.ru>
2023-10-13 21:49:19 +0300

    wrong answer

--- leetcode_problems/94_page/solutions/remove-k-digits/src/main.rs
+++ leetcode_problems/94_page/solutions/remove-k-digits/src/main.rs
@@ -2,54 +2,89 @@ struct Solution;
 
 impl Solution {
     pub fn remove_kdigits(num: String, k: i32) -> String {
+        let zero_num = "0".to_string();
         if k as usize == num.len() || num.len() == 1 {
-            return "0".to_string()
+            return zero_num;
         }
         let mut result_num = Vec::with_capacity(num.len() - k as usize);
         let mut i = 0;
         let mut count = 0;
-        for (j, pair) in num.as_bytes().windows(2).enumerate() {
-            if pair[0] > pair[1] {
-                while let Some(c) = result_num.last() {
-                    if *c > pair[1] && count < k {
-                        result_num.pop();
-                    } else {
-                        break;
-                    }
+        for (j, num_char) in num.as_bytes().windows(2).enumerate() {
+            if num_char[0] > num_char[1] {
+                loop {
                     count += 1;
+                    match result_num.last() {
+                        Some(c) if *c > num_char[1] && count < k => {
+                            result_num.pop();
+                        },
+                        Some(_) | None => break,
+                    }
                 }
             } else {
-                result_num.push(pair[0]);
-                if j == num.len() - 2 {
-                    result_num.push(pair[1]);
-                }
+                result_num.push(num_char[0]);
             }
             if count == k {
-                i = j + 1;
+                if result_num.len() > 0 {
+                    i = j + 2;
+                } else {
+                    i = j + 1;
+                }
+                break;
+            } else if result_num.len() > 0 && j == num.len() - 1 - k as usize {
                 break;
             }
         }
-        while count < k {
-            result_num.pop();
-            count += 1;
+
+        if result_num.len() == 0 && i == 0 {
+            return zero_num;
         }
-        if i > 0 {
-            result_num.extend_from_slice(&num.as_bytes()[i..]);
-        }
-        let mut result = std::str::from_utf8(&result_num[..])
-            .unwrap()
+
+        let num_str = if result_num.len() == 0 {
+            &num.as_str()[i..]
+        } else {
+            if i > 0 {
+                result_num.extend_from_slice(&num.as_bytes()[i..]);
+            }
+            std::str::from_utf8(&result_num[..]).unwrap()
+        };
+
+        let mut num = num_str
             .chars()
             .skip_while(|c| *c == '0')
             .collect::<String>();
-        if result.len() == 0 {
-            result.push('0')
+        if num.len() == 0 {
+            num = zero_num;
         }
-        result
+        num
+
     }
 }
 
 fn main() {
-    let k = 2;
-    let num = String::from("12345");
-    assert_eq!(String::from("123"), Solution::remove_kdigits(num, k));
+    let mut k;
+    let mut num;
+
+    k = 3;
+    num = String::from("1432219");
+    assert_eq!(String::from("1219"), Solution::remove_kdigits(num, k));
+
+    // k = 2;
+    // num = String::from("12345");
+    // assert_eq!(String::from("123"), Solution::remove_kdigits(num, k));
+    //
+    // k = 1;
+    // num = String::from("112");
+    // assert_eq!(String::from("11"), Solution::remove_kdigits(num, k));
+    //
+    // k = 1;
+    // num = String::from("102");
+    // assert_eq!(String::from("2"), Solution::remove_kdigits(num, k));
+    //
+    // k = 1;
+    // num = String::from("100");
+    // assert_eq!(String::from("0"), Solution::remove_kdigits(num, k));
+    //
+    // k = 2;
+    // num = String::from("54321");
+    // assert_eq!(String::from("321"), Solution::remove_kdigits(num, k));
 }
