af4ea19c0d72cb7845da0ed899015508867c1747
Ildar Shamiev <shamief@yandex.ru>
2023-10-27 13:50:23 +0300

    wrong solution

--- leetcode_problems/137_page/solutions/validate-binary-search-tree/src/main.rs
+++ leetcode_problems/137_page/solutions/validate-binary-search-tree/src/main.rs
@@ -50,8 +50,18 @@ impl Solution {
         let mut queue = VecDeque::new();
         let mut max_val = root_val;
         let mut min_val = root_val;
+        let mut next_val = root_val;
         queue.push_back(tree.clone());
         while let Some(node) = queue.pop_front() {
+            if is_left_tree {
+                if min_val < root_val && node.borrow().val < min_val {
+                    min_val = root_val;
+                }
+                if next_val < root_val && node.borrow().val < next_val {
+                    max_val = next_val;
+                    next_val = root_val;
+                }
+            }
             if let Some(ref left) = node.borrow().left {
                 if left.borrow().val >= node.borrow().val {
                     return false;
@@ -76,12 +86,15 @@ impl Solution {
                 if max_val != root_val && right.borrow().val >= max_val {
                     return false;
                 }
+                if node.borrow().left.is_some() {
+                    next_val = node.borrow().val;
+                }
                 if is_left_tree {
                     queue.push_front(right.clone());
                 } else {
                     queue.push_back(right.clone());
                 }
-            } else {
+            } else if node.borrow().left.is_some() {
                 max_val = node.borrow().val;
             }
         }
@@ -161,115 +174,136 @@ fn main() {
     // root.left = Some(Rc::new(RefCell::new(left_child)));
     // assert!(!(Solution::is_valid_bst(Some(Rc::new(RefCell::new(root))))));
 
-    // [98,97,null,88,null,84,null,79,87,64,null,null,null,63,69,62,null,null,null,30,
-    // null,27,59,9,null,null,null,3,null,0,null,-4,null,-16,null,-18,-7,-19,null,null,
-    // null,-23,null,-34,null,-42,null,-59,null,-63,null,-64,null,-69,null,-75,null,-81]
-    //
-    //                                                              98
-    //                                                             /
-    //                                                            97
-    //                                                           /
-    //                                                          88
-    //                                                         /
-    //                                                        84
-    //                                                       /  \
-    //                                                      79  87
-    //                                                     /
-    //                                                    64
-    //                                                   /  \
-    //                                                  63  69
-    //                                                 /
-    //                                                62
-    //                                               /
-    //                                              30
-    //                                             /  \
-    //                                            27  59
-    //                                           /
-    //                                          9
-    //                                         /
-    //                                        3
-    //                                       /
-    //                                      0
+
+    //                                              90
+    //                                             /
+    //                                            84
+    //                                           /  \
+    //                                          79  87
+    //                                         /  \  \
+    //                                        64  85  88
+    //                                       /  \      \
+    //                                      63  69     89
     //                                     /
-    //                                    -4
+    //                                    62
     //                                   /
-    //                                 -16
+    //                                  30
     //                                 /  \
-    //                               -18  -7
+    //                                27  59
     //                               /
-    //                             -19
+    //                              9
     //                             /
-    //                           -23
+    //                            3
     //                           /
-    //                         -34
+    //                          0
     //                         /
-    //                       -42
+    //                        -4
     //                       /
-    //                     -59
-    //                     /
-    //                   -63
+    //                     -16
+    //                     /  \
+    //                   -18  -7
     //                   /
-    //                 -64
+    //                 -19
     //                 /
-    //               -69
+    //               -23
     //               /
-    //             -75
+    //             -34
     //             /
-    //           -81
-    let node25 = TreeNode::new(-81);
-    let mut node24 = TreeNode::new(-75);
-    node24.left = Some(Rc::new(RefCell::new(node25)));
-    let mut node23 = TreeNode::new(-69);
-    node23.left = Some(Rc::new(RefCell::new(node24)));
-    let mut node22 = TreeNode::new(-64);
-    node22.left = Some(Rc::new(RefCell::new(node23)));
-    let mut node21 = TreeNode::new(-63);
-    node21.left = Some(Rc::new(RefCell::new(node22)));
-    let mut node20 = TreeNode::new(-59);
-    node20.left = Some(Rc::new(RefCell::new(node21)));
-    let mut node19 = TreeNode::new(-42);
-    node19.left = Some(Rc::new(RefCell::new(node20)));
-    let mut node18 = TreeNode::new(-34);
-    node18.left = Some(Rc::new(RefCell::new(node19)));
-    let mut node17 = TreeNode::new(-23);
-    node17.left = Some(Rc::new(RefCell::new(node18)));
-    let mut node16 = TreeNode::new(-19);
-    node16.left = Some(Rc::new(RefCell::new(node17)));
-    let mut node15 = TreeNode::new(-18);
-    node15.left = Some(Rc::new(RefCell::new(node16)));
-    let mut node14 = TreeNode::new(-16);
-    node14.left = Some(Rc::new(RefCell::new(node15)));
-    node14.right = Some(Rc::new(RefCell::new(TreeNode::new(-7))));
-    let mut node13 = TreeNode::new(-4);
-    node13.left = Some(Rc::new(RefCell::new(node14)));
-    let mut node12 = TreeNode::new(0);
-    node12.left = Some(Rc::new(RefCell::new(node13)));
-    let mut node11 = TreeNode::new(3);
-    node11.left = Some(Rc::new(RefCell::new(node12)));
-    let mut node10 = TreeNode::new(9);
-    node10.left = Some(Rc::new(RefCell::new(node11)));
-    let mut node9 = TreeNode::new(27);
-    node9.left = Some(Rc::new(RefCell::new(node10)));
-    let mut node8 = TreeNode::new(30);
-    node8.left = Some(Rc::new(RefCell::new(node9)));
-    node8.right = Some(Rc::new(RefCell::new(TreeNode::new(59))));
-    let mut node7 = TreeNode::new(62);
-    node7.left = Some(Rc::new(RefCell::new(node8)));
-    let mut node6 = TreeNode::new(63);
-    node6.left = Some(Rc::new(RefCell::new(node7)));
-    let mut node5 = TreeNode::new(64);
-    node5.left = Some(Rc::new(RefCell::new(node6)));
-    node5.right = Some(Rc::new(RefCell::new(TreeNode::new(69))));
-    let mut node4 = TreeNode::new(79);
-    node4.left = Some(Rc::new(RefCell::new(node5)));
-    let mut node3 = TreeNode::new(84);
-    node3.left = Some(Rc::new(RefCell::new(node4)));
-    node3.right = Some(Rc::new(RefCell::new(TreeNode::new(87))));
-    let mut node2 = TreeNode::new(88);
-    node2.left = Some(Rc::new(RefCell::new(node3)));
-    let mut node1 = TreeNode::new(97);
+    //           -42
+    //           /
+    //         -59
+    //         /
+    //       -63
+    //       /
+    //     -64
+    //     /
+    //   -69
+    //   /
+    // -75
+    // /
+    //-81
+    // let node25 = TreeNode::new(-81);
+    // let mut node24 = TreeNode::new(-75);
+    // node24.left = Some(Rc::new(RefCell::new(node25)));
+    // let mut node23 = TreeNode::new(-69);
+    // node23.left = Some(Rc::new(RefCell::new(node24)));
+    // let mut node22 = TreeNode::new(-64);
+    // node22.left = Some(Rc::new(RefCell::new(node23)));
+    // let mut node21 = TreeNode::new(-63);
+    // node21.left = Some(Rc::new(RefCell::new(node22)));
+    // let mut node20 = TreeNode::new(-59);
+    // node20.left = Some(Rc::new(RefCell::new(node21)));
+    // let mut node19 = TreeNode::new(-42);
+    // node19.left = Some(Rc::new(RefCell::new(node20)));
+    // let mut node18 = TreeNode::new(-34);
+    // node18.left = Some(Rc::new(RefCell::new(node19)));
+    // let mut node17 = TreeNode::new(-23);
+    // node17.left = Some(Rc::new(RefCell::new(node18)));
+    // let mut node16 = TreeNode::new(-19);
+    // node16.left = Some(Rc::new(RefCell::new(node17)));
+    // let mut node15 = TreeNode::new(-18);
+    // node15.left = Some(Rc::new(RefCell::new(node16)));
+    // let mut node14 = TreeNode::new(-16);
+    // node14.left = Some(Rc::new(RefCell::new(node15)));
+    // node14.right = Some(Rc::new(RefCell::new(TreeNode::new(-7))));
+    // let mut node13 = TreeNode::new(-4);
+    // node13.left = Some(Rc::new(RefCell::new(node14)));
+    // let mut node12 = TreeNode::new(0);
+    // node12.left = Some(Rc::new(RefCell::new(node13)));
+    // let mut node11 = TreeNode::new(3);
+    // node11.left = Some(Rc::new(RefCell::new(node12)));
+    // let mut node10 = TreeNode::new(9);
+    // node10.left = Some(Rc::new(RefCell::new(node11)));
+    // let mut node9 = TreeNode::new(27);
+    // node9.left = Some(Rc::new(RefCell::new(node10)));
+    // let mut node8 = TreeNode::new(30);
+    // node8.left = Some(Rc::new(RefCell::new(node9)));
+    // node8.right = Some(Rc::new(RefCell::new(TreeNode::new(59))));
+    // let mut node7 = TreeNode::new(62);
+    // node7.left = Some(Rc::new(RefCell::new(node8)));
+    // let mut node6 = TreeNode::new(63);
+    // node6.left = Some(Rc::new(RefCell::new(node7)));
+    // let mut node5 = TreeNode::new(64);
+    // node5.left = Some(Rc::new(RefCell::new(node6)));
+    // node5.right = Some(Rc::new(RefCell::new(TreeNode::new(69))));
+    // let mut node4 = TreeNode::new(79);
+    // node4.left = Some(Rc::new(RefCell::new(node5)));
+    // node4.right = Some(Rc::new(RefCell::new(TreeNode::new(85))));
+    // let mut node3_4_2 = TreeNode::new(88);
+    // node3_4_2.right = Some(Rc::new(RefCell::new(TreeNode::new(89))));
+    // let mut node3_4_1 = TreeNode::new(87);
+    // node3_4_1.right = Some(Rc::new(RefCell::new(node3_4_2)));
+    // let mut node3 = TreeNode::new(84);
+    // node3.left = Some(Rc::new(RefCell::new(node4)));
+    // node3.right = Some(Rc::new(RefCell::new(node3_4_1)));
+    // root = TreeNode::new(90);
+    // root.left = Some(Rc::new(RefCell::new(node3)));
+    // assert!(!Solution::is_valid_bst(Some(Rc::new(RefCell::new(root)))));
+
+   //    80
+   //     \
+   //     86
+   //    /  \
+   //   83  87
+   //  /   /  \
+   // 82  84  88
+   //  \       \
+   //  85      89
+
+    let mut node5 = TreeNode::new(88);
+    node5.right = Some(Rc::new(RefCell::new(TreeNode::new(89))));
+    let mut node3 = TreeNode::new(87);
+    node3.left = Some(Rc::new(RefCell::new(TreeNode::new(84))));
+    node3.right = Some(Rc::new(RefCell::new(node5)));
+    let mut node4 = TreeNode::new(82);
+    node4.right = Some(Rc::new(RefCell::new(TreeNode::new(85))));
+    let mut node2 = TreeNode::new(83);
+    node2.left = Some(Rc::new(RefCell::new(node4)));
+    let mut node1 = TreeNode::new(86);
     node1.left = Some(Rc::new(RefCell::new(node2)));
-    root = TreeNode::new(98);
-    root.left = Some(Rc::new(RefCell::new(node1)));
-    assert!(Solution::is_valid_bst(Some(Rc::new(RefCell::new(root)))));
+    node1.right = Some(Rc::new(RefCell::new(node3)));
+    root = TreeNode::new(80);
+    root.right = Some(Rc::new(RefCell::new(node1)));
+    assert!(!Solution::is_valid_bst(Some(Rc::new(RefCell::new(root)))));
+
 }
